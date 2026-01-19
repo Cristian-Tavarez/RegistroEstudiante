ad786e79e6dbdee050a7e6e8e80626d8c8f20095
cristian tavarez <cristian22041@gmail.com>
2026-01-19 12:10:22 -0400

    El program con todo lo agregado del proyecto

--- RegistroEstudiante/Program.cs
+++ RegistroEstudiante/Program.cs
@@ -1,23 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroEstudiante.Contexto;
 using RegistroEstudiante.Components;
+using RegistroEstudiante.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// 🔹 REGISTRO DE SERVICIOS
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddDbContext<EstudiantesContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// 🔴 ESTA LÍNEA FALTABA
+builder.Services.AddScoped<EstudiantesService>();
+
+// 🔹 AQUÍ recién se construye la app
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// 🔹 MIDDLEWARE
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
-
 app.UseAntiforgery();
 
 app.MapStaticAssets();
