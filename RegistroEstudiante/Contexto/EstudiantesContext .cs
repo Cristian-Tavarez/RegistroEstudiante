dd3780b6b8da6c85a58bda5e18377c29507ac864
cristian tavarez <cristian22041@gmail.com>
2026-01-19 12:07:27 -0400

    Creacion del Contexto

---
+++ RegistroEstudiante/Contexto/EstudiantesContext .cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroEstudiante.Models;
+
+namespace RegistroEstudiante.Contexto;
+
+public class EstudiantesContext : DbContext
+{
+    public EstudiantesContext(DbContextOptions<EstudiantesContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Estudiante> Estudiantes { get; set; }
+}
