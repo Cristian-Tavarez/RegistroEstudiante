e7aa5247446b27ee0dfbb7eaa058f2b18a550eb1
cristian tavarez <cristian22041@gmail.com>
2026-01-19 12:02:14 -0400

    Creacion del modelo

---
+++ RegistroEstudiante/Models/Estudiante.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace RegistroEstudiante.Models;
+
+public class Estudiante
+{
+    [Key]
+    public int EstudianteId { get; set; }
+
+    [Required(ErrorMessage = "El nombre es obligatorio")]
+    public string Nombres { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "El apellido es obligatorio")]
+    public string Apellidos { get; set; } = string.Empty;
+
+    [Required]
+    public string Matricula { get; set; } = string.Empty;
+
+    [Required]
+    public string Carrera { get; set; } = string.Empty;
+
+    public DateTime FechaIngreso { get; set; } = DateTime.Now;
+}
