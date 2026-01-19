38c0d4d35a34ae0d487807ec401a2d1b6452e2e0
cristian tavarez <cristian22041@gmail.com>
2026-01-19 12:08:58 -0400

    Creacion del Service

---
+++ RegistroEstudiante/Services/EstudiantesService .cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroEstudiante.Contexto;
+using RegistroEstudiante.Models;
+
+namespace RegistroEstudiante.Services;
+
+public class EstudiantesService
+{
+    private readonly EstudiantesContext _context;
+
+    public EstudiantesService(EstudiantesContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Estudiante>> ListarAsync()
+    {
+        return await _context.Estudiantes.ToListAsync();
+    }
+
+    public async Task<List<Estudiante>> BuscarAsync(string texto)
+    {
+        if (string.IsNullOrWhiteSpace(texto))
+            return await _context.Estudiantes.ToListAsync();
+
+        texto = texto.ToLower();
+
+        return await _context.Estudiantes
+            .Where(e =>
+                e.Nombres.ToLower().Contains(texto) ||
+                e.Apellidos.ToLower().Contains(texto) ||
+                e.Matricula.ToLower().Contains(texto)
+            )
+            .ToListAsync();
+    }
+    public async Task<Estudiante?> BuscarAsync(int id)
+    {
+        return await _context.Estudiantes.FindAsync(id);
+    }
+
+    public async Task GuardarAsync(Estudiante estudiante)
+    {
+        if (estudiante.EstudianteId == 0)
+            _context.Estudiantes.Add(estudiante);
+        else
+            _context.Estudiantes.Update(estudiante);
+
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task EliminarAsync(int id)
+    {
+        var estudiante = await BuscarAsync(id);
+        if (estudiante != null)
+        {
+            _context.Estudiantes.Remove(estudiante);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
