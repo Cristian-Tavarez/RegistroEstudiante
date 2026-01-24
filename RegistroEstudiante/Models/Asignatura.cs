8e1cf54b3d82d02cbfad55111d70e65a152e1f63
cristian tavarez <cristian22041@gmail.com>
2026-01-23 20:12:39 -0400

    Creacion del modelo Asignatura

---
+++ RegistroEstudiante/Models/Asignatura.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroEstudiante.Models;
+
+public class Asignatura
+{
+    [Key]
+    public int AsignaturaId { get; set; }
+
+    [Required(ErrorMessage = "El código es obligatorio")]
+    public string Codigo { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "El nombre es obligatorio")]
+    public string Nombre { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "El aula es obligatoria")]
+    public string Aula { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Los créditos son obligatorios")]
+    [Range(1, 10, ErrorMessage = "Créditos inválidos")]
+    public int Creditos { get; set; }
+}
